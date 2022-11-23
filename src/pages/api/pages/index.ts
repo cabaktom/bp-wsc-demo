8549c2fe2f303084ff88505f60d0157501ee5d81
cabaktom <tomascabak00@gmail.com>
2022-11-23 21:51:08 +0100

    Add pages API

---
+++ src/pages/api/pages/index.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../lib/prisma';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    const pages = await prisma.page.findMany();
+    res.status(200).json(pages);
+  } else {
+    res.status(405).setHeader('Allow', 'GET').end();
+  }
+}
