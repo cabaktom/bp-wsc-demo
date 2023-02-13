dbcab28f9fc3ee5c5d849e6cf322b2909c4752cd
cabaktom <tomascabak00@gmail.com>
2023-02-13 14:41:57 +0100

    Add search to list of participants

--- src/components/List/ParticipantList.tsx
+++ src/components/List/ParticipantList.tsx
@@ -1,8 +1,17 @@
-import { Flex, Text, ThemeIcon, Title, createStyles } from '@mantine/core';
+import {
+  CloseButton,
+  Flex,
+  Text,
+  TextInput,
+  ThemeIcon,
+  Title,
+  createStyles,
+} from '@mantine/core';
 import type { Abstract, Participant } from '@prisma/client';
 import { IconUserCircle } from '@tabler/icons-react';
 
 import MyPaper from '../Layout/MyPaper';
+import useSearch from '../../hooks/useSearch';
 
 const useStyles = createStyles((theme) => ({
   list: {
@@ -32,71 +41,83 @@ type ParticipantListProps = {
 const ParticipantList = ({ participants }: ParticipantListProps) => {
   const { classes } = useStyles();
 
-  return (
-    <ul className={classes.list}>
-      {participants.map(({ abstract, ...participant }) => (
-        <li key={participant.fullName}>
-          <Flex
-            direction={{ base: 'column', md: 'row' }}
-            gap={{ base: 0, md: 'sm' }}
-          >
-            <Flex
-              direction="row"
-              gap="sm"
-              w={{ base: '100%', md: '30%' }}
-              my={{ base: 'xs', md: 'sm' }}
-              mx={{ base: 'xs', md: 0 }}
-            >
-              <ThemeIcon size={30} radius="md">
-                <IconUserCircle />
-              </ThemeIcon>
-              <Flex direction="column">
-                <Text weight="bold" size="lg">
-                  {participant.fullName}
-                </Text>
-                {participant.affiliation}
-              </Flex>
-            </Flex>
+  const { query, setQuery, searchResults } = useSearch({ data: participants });
 
-            <MyPaper
-              w={{ base: '100%', md: '70%' }}
-              bg={abstract ? '' : 'gray.0'}
+  return (
+    <>
+      <TextInput
+        label="Search"
+        placeholder="Search participants and abstracts"
+        value={query}
+        onChange={(event) => setQuery(event.currentTarget.value)}
+        rightSection={<CloseButton size={18} onClick={() => setQuery('')} />}
+      />
+
+      <ul className={classes.list}>
+        {searchResults.map(({ abstract, ...participant }) => (
+          <li key={participant.fullName}>
+            <Flex
+              direction={{ base: 'column', md: 'row' }}
+              gap={{ base: 0, md: 'sm' }}
             >
-              {abstract && (
-                <>
-                  <Title order={4}>{abstract.title}</Title>
-                  <dl className={classes.definitionList}>
-                    {abstract.additionalAuthors && (
-                      <>
-                        <dt>Additional authors: </dt>
-                        <dd>{abstract.additionalAuthors}</dd>
-                      </>
-                    )}
-                    {abstract.affiliationAuthors && (
-                      <>
-                        <dt>Additional authors&apos; affiliation:</dt>
-                        <dd>{abstract.affiliationAuthors}</dd>
-                      </>
-                    )}
-                    {abstract.abstract && (
-                      <>
-                        <dt>Abstract: </dt>
-                        <dd>{abstract.abstract}</dd>
-                      </>
-                    )}
-                  </dl>
-                </>
-              )}
-              {!abstract && (
-                <Flex align="center" h="100%">
-                  No abstract submitted.
+              <Flex
+                direction="row"
+                gap="sm"
+                w={{ base: '100%', md: '30%' }}
+                my={{ base: 'xs', md: 'sm' }}
+                mx={{ base: 'xs', md: 0 }}
+              >
+                <ThemeIcon size={30} radius="md">
+                  <IconUserCircle />
+                </ThemeIcon>
+                <Flex direction="column">
+                  <Text weight="bold" size="lg">
+                    {participant.fullName}
+                  </Text>
+                  {participant.affiliation}
                 </Flex>
-              )}
-            </MyPaper>
-          </Flex>
-        </li>
-      ))}
-    </ul>
+              </Flex>
+
+              <MyPaper
+                w={{ base: '100%', md: '70%' }}
+                bg={abstract ? '' : 'gray.0'}
+              >
+                {abstract && (
+                  <>
+                    <Title order={4}>{abstract.title}</Title>
+                    <dl className={classes.definitionList}>
+                      {abstract.additionalAuthors && (
+                        <>
+                          <dt>Additional authors: </dt>
+                          <dd>{abstract.additionalAuthors}</dd>
+                        </>
+                      )}
+                      {abstract.affiliationAuthors && (
+                        <>
+                          <dt>Additional authors&apos; affiliation:</dt>
+                          <dd>{abstract.affiliationAuthors}</dd>
+                        </>
+                      )}
+                      {abstract.abstract && (
+                        <>
+                          <dt>Abstract: </dt>
+                          <dd>{abstract.abstract}</dd>
+                        </>
+                      )}
+                    </dl>
+                  </>
+                )}
+                {!abstract && (
+                  <Flex align="center" h="100%">
+                    No abstract submitted.
+                  </Flex>
+                )}
+              </MyPaper>
+            </Flex>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
