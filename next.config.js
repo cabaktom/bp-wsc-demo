70921ac662d01530b29c58e45976b49b3ecf1f86
cabaktom <tomascabak00@gmail.com>
2023-04-09 23:36:39 +0200

    Add Dockerfile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: false, // because of react-beautiful-dnd issue https://github.com/atlassian/react-beautiful-dnd/issues/2350
   swcMinify: true,
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
